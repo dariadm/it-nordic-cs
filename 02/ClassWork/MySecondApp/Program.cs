ee72c17b221dc7094a756b05b991c03515378ced
Daria Volkova <daria911@yandex.ru>
2019-02-25 21:40:09 +0300

    Class Work Lesson 2 added

---
+++ 02/ClassWork/MySecondApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MySecondApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//char letter = 'A';
+			//Console.WriteLine(letter);
+
+			//string name = "Bob";
+			//name = name.ToUpper().ToLower(); 
+			//Console.WriteLine(name.ToUpper());
+			//Console.WriteLine(name);
+
+			//System.Int32 myInteger;
+
+			//float x1 = 3.5F;
+			//Console.WriteLine(x1);
+
+			//int x = 3;
+			//float y = float.NegativeInfinity;//4.5F;
+			//short z = 5;
+			//var result = x + y / z;
+			//Console.WriteLine("The result is {0}", result);
+			//Type type = result.GetType();
+			//Console.WriteLine("result is of type {0}", type.ToString());
+
+			//float pi = Convert.ToSingle(Math.PI);
+
+			float notNumber = float.NaN;
+			float zero = 0;
+			Console.WriteLine("{0} / {0} = {1}", zero, zero / zero);
+
+			Console.WriteLine(zero / zero == float.NaN);
+			Console.WriteLine(float.IsNaN(zero / zero));
+
+			Console.WriteLine(float.PositiveInfinity);
+			Console.WriteLine(float.NegativeInfinity);
+
+		}
+	}
+}
---
+++ 02/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите радиус: ");
+			double radius = double.Parse(Console.ReadLine());
+			double circleS = Math.PI * Math.Pow(radius, 2);
+			Console.WriteLine("Площадь круга: {0}", circleS);
+		}
+	}
+}

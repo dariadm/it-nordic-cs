91f3dd1651515ebd670e779d463641bdc87eb39d
Daria Volkova <daria911@yandex.ru>
2019-03-03 21:06:18 +0300

    HomeWork Lesson 3 added

---
+++ 03/HomeWork/HomeApp03/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace HomeApp03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("***********Your age in 4 years***********");
+
+            string[] userNames = new string[3];
+            byte[] userAges = new byte[3];
+
+            // Reading users' names
+            for (uint i = 0; i < userNames.Length; ++i)
+            {
+                Console.WriteLine($"Please, enter {i+1} name:");
+                userNames[i] = Console.ReadLine();
+            }
+ 
+            // Reading users' ages
+            for (uint i = 0; i < userAges.Length; ++i)
+            {
+                Console.WriteLine($"Please, enter {i+1} age (full age):");
+                userAges[i] = ReadAge();
+            }
+
+            // Writing users' info
+            Console.WriteLine("");
+            Console.WriteLine("Counted age of users:");
+            for (uint i = 0; i < userNames.Length; ++i)
+            {
+                Console.WriteLine("Name: {0}, age in 4 years: {1}", userNames[i], (userAges[i] + 4));
+            }
+            
+            Console.ReadKey();
+        }
+
+        // Reading age
+        static byte ReadAge()
+        {
+            byte age;
+
+            while (true)
+            {
+                if (byte.TryParse(Console.ReadLine(), out age))
+                    break;
+
+                Console.WriteLine("Wrong value! ");
+            }
+
+            return age;
+        }
+    }
+}

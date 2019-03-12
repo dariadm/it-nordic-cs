17f5f409673ed0c3a604415c345bf5af51a7a6b1
Daria Volkova <daria911@yandex.ru>
2019-03-12 22:44:05 +0300

    Homework lesson 6 added

---
+++ 06/HomeWork/HomeApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace HomeApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("***********Counting even digits in positive integer***********");
+
+            int userNumber = 0;
+            uint evenDigitsNumber = 0; 
+
+            Console.WriteLine("Enter positive integer, not bigger then 2000000000:");
+
+            while (true)
+            {
+                try
+                {
+                    userNumber = int.Parse(Console.ReadLine());
+
+                    // Checking if positive
+                    if (userNumber > 0)
+                        break;
+
+                    // If negative
+                    Console.WriteLine("Wrong value! Try again");
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"Error {e.GetType()}! Try again");
+                    continue;
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine($"Error {e.GetType()}! Try again");
+                    continue;
+                }
+            }
+
+            // Counting positive digits
+            int rem = 0;
+            int userNumberCopy = userNumber; 
+            while (userNumberCopy != 0)
+            {
+                // Getting next digit
+                rem = userNumberCopy / 10;
+
+                // Checking if even
+                if (rem % 2 == 0)
+                    evenDigitsNumber += 1;
+
+                // Reducing digit
+                userNumberCopy = (userNumberCopy - rem) / 10; 
+            }
+
+            Console.WriteLine("The amount of even digits in number {0} is {1}", userNumber, evenDigitsNumber);
+        }
+    }
+}
---
+++ 06/HomeWork/HomeApp2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace HomeApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("***********Counting days to get sum***********");
+
+            int initialPayment = 0;
+            double dayPercentOfIncome = 0;
+            int desiredAmount = 0;
+            int daysAmount = 0;
+
+            // Getting data 
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter the initial payment:");
+                    initialPayment = int.Parse(Console.ReadLine());
+                    if (initialPayment <= 0)
+                    {
+                        Console.WriteLine("Wrong value! Try again");
+                        continue;
+                    }
+
+                    break;
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Enter the day percent of income as decimal with \".\" :");
+                    string tempDayPercentOfIncome = Console.ReadLine();
+                    if (tempDayPercentOfIncome.IndexOf('.') == -1)
+                    {
+                        Console.WriteLine("Wrong format! Try again");
+                        continue;
+                    }
+                    dayPercentOfIncome = double.Parse(tempDayPercentOfIncome);
+                    if (dayPercentOfIncome <= 0)
+                    {
+                        Console.WriteLine("Wrong value! Try again");
+                        continue;
+                    }
+
+                    break;
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Enter the desired amount:");
+                    desiredAmount = int.Parse(Console.ReadLine());
+                    if (desiredAmount <= 0)
+                    {
+                        Console.WriteLine("Wrong value! Try again");
+                        continue;
+                    }
+
+                    break;
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Error {e.GetType()}! Finish");
+                throw;
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine($"Error {e.GetType()}! Finish");
+                throw;
+            }
+
+            // Counting days
+            double currentSum = (double)initialPayment; 
+            while(currentSum <= desiredAmount)
+            {
+                currentSum += currentSum * dayPercentOfIncome;
+                daysAmount += 1; 
+            }
+
+            Console.WriteLine($"The amount of days to get desired amount is {daysAmount}");
+        }
+    }
+}

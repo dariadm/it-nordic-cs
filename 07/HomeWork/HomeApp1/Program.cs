b0d2d4abfb80917fb6b75f7b375259f63f40a25b
Daria Volkova <daria911@yandex.ru>
2019-03-18 14:48:40 +0300

    HomeWork lesson 7 added

---
+++ 07/HomeWork/HomeApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace HomeApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("***********Counting words starting with english letter 'A' in user's string***********");
+
+            string userString = "";
+            string[] wordsInUserString;
+            uint wordsStartingWithA = 0;
+
+            Console.WriteLine("Enter the string (must be not less then 2 words):");
+            // Getting string and checking if enough words
+            while (true)
+            {
+                userString = Console.ReadLine();
+
+                wordsInUserString = userString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (wordsInUserString.Length >= 2)
+                    break;
+
+                Console.WriteLine("Not enough words! Try again and enter at less 2");
+            }
+
+            // Counting letter A
+            foreach (string s in wordsInUserString)
+            {
+                if (s[0] == 'A' || s[0] == 'a')
+                    wordsStartingWithA += 1; 
+            }
+
+            Console.WriteLine($"The number of words starting with english 'A' is: {wordsStartingWithA}");
+        }
+    }
+}
---
+++ 07/HomeWork/HomeApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace HomeApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("***********Printing user's string in lower case and reverse order***********");
+
+            string userString = "";
+            string[] wordsInUserString;
+
+            Console.WriteLine("Enter the string:");
+            // Getting string and checking if enough words
+            while (true)
+            {
+                userString = Console.ReadLine();
+
+                wordsInUserString = userString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (wordsInUserString.Length >= 1)
+                    break;
+
+                Console.WriteLine("The strig is empty! Try again");
+            }
+
+            // Converting
+            string userStringConverted = "";
+            for (int i = userString.Length - 1; i >= 0; --i)
+            {
+                userStringConverted += (userString.ToLower())[i];
+            }
+
+            Console.WriteLine(userStringConverted);
+        }
+    }
+}

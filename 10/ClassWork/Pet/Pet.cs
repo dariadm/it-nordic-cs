f1bc3d3771fe73d0e063920a4630704499b49fdb
Daria Volkova <daria911@yandex.ru>
2019-03-25 21:30:06 +0300

    ClassWork Lesson 10 added

---
+++ 10/ClassWork/Pet/Pet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Pet
+{
+	public string Kind;
+	public string Name;
+	public char Sex;
+	public uint Age;
+
+	public string Description
+	{
+		get
+		{
+			return $"{Name} is a {Kind}({Sex}) of {Age} years old";
+		}
+	}
+}
---
+++ 10/ClassWork/Pet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet pet1 = new Pet();
+			pet1.Name = "Barsik";
+			pet1.Kind = "Cat";
+			pet1.Age = 2;
+			pet1.Sex = 'M';
+
+			Console.WriteLine(pet1.Description);
+
+			Pet pet2 = new Pet
+			{
+				Name = "Sharik",
+				Kind = "Dog",
+				Age = 3,
+				Sex = 'M'
+			};
+
+			Console.WriteLine(pet2.Description);
+		}
+	}
+}

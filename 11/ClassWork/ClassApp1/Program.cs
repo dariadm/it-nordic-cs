313cd5ca2f65119a977fad0a07cc2c690ed281b1
Daria Volkova <daria911@yandex.ru>
2019-03-27 21:36:11 +0300

    ClassWork Lesson 11 added

---
+++ 11/ClassWork/ClassApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet pet1 = new Pet();
+			pet1.Name = "Barsik";
+			pet1.Kind = "Cat";
+			pet1.DateOfBirth = DateTimeOffset.Parse("2017-02-02");
+			pet1.Sex = 'M';
+
+			pet1.WriteDescription(false);
+
+			Pet pet2 = new Pet
+			{
+				Name = "Sharik",
+				Kind = "Dog",
+				DateOfBirth = DateTimeOffset.Parse("2016-02-02"),
+				Sex = 'M'
+			};
+
+			pet2.WriteDescription(true);
+		}
+	}
+}

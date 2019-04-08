6eb5829398ececa454fc09e6d9f9dadf945cbf2e
Daria Volkova <daria911@yandex.ru>
2019-04-08 21:27:04 +0300

    ClassWork lesson 14 added

---
+++ 14/ClassApp1/ErrorList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+class ErrorList : IDisposable, IEnumerable<string>
+{
+	public static string OutPrefixFormat { get; set; }
+	public string Category { get; private set; }
+	private List<string> _errors;
+
+	static ErrorList()
+	{
+		OutPrefixFormat = "yyyy-MM-ddTHH:mm:ss";
+		//OutPrefixFormat = "M d, yyyy (hh:mm)";
+	}
+
+	public ErrorList(string category)
+	{
+		Category = category;
+		_errors = new List<string>();
+	}
+
+	public void Add(string errorMessage)
+	{
+		_errors.Add(errorMessage);
+	}
+
+	public void WriteToConsole()
+	{
+		foreach (var error in _errors)
+			Console.WriteLine(DateTime.Now.ToString(OutPrefixFormat) + $"	{Category}" + $"	{error}");
+	}
+
+	public void Dispose()
+	{
+		if (_errors != null)
+		{
+			_errors.Clear();
+			_errors = null;
+		}
+	}
+
+	public IEnumerator<string> GetEnumerator()
+	{
+		return _errors.GetEnumerator();
+	}
+
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return _errors.GetEnumerator();
+	}
+}
---
+++ 14/ClassApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using(var myErrorList = new ErrorList("Stackoverflow error"))
+			{
+				myErrorList.Add("Bad error 1");
+				myErrorList.Add("Bad error 2");
+
+				// Writing
+				//foreach (var error in myErrorList)
+				//	Console.WriteLine($"{myErrorList.Category} : {error}");
+
+				myErrorList.WriteToConsole();
+			}
+		}
+	}
+}

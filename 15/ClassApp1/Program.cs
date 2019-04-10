15f4eba0998139a1b4a07795486caca0a51f5c56
Daria Volkova <daria911@yandex.ru>
2019-04-10 21:30:07 +0300

    CkassWork Lesson 15 added

---
+++ 15/ClassApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Account<int> firstClient = new Account<int>(23424, "Name1");
+			Account<string> secondClient = new Account<string>("sf2243", "Name2");
+			Account<Guid> thirdClient = new Account<Guid>(Guid.NewGuid(), "Name3");
+
+			firstClient.WriteProperties();
+			secondClient.WriteProperties();
+			thirdClient.WriteProperties();
+
+			Console.WriteLine(thirdClient.getId());
+		}
+	}
+}
---
+++ ClassApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Circle myCircle = new Circle(1.5);
+			double circleSquare = myCircle.Calculate(Calculation.CalcSquare);
+			double circlePerimetr = myCircle.Calculate(Calculation.CalcPerimetr);
+
+			Console.WriteLine($"circlePerimetr {circlePerimetr}");
+			Console.WriteLine($"circleSquare {circleSquare}");
+		}
+	}
+}
